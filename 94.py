ab1d388ae5b7c75efacfbadaeeeaf2802dc6087f
Kevin Broadbridge <kevinbroadbridge1@gmail.com>
2020-08-27 23:07:09 -0500

    94(recursive)

---
+++ 94.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+# Recursive function. TODO: iterative
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        list = []
+        if root is None:
+            return None
+        if root.left:
+            list +=self.inorderTraversal(root.left)
+        list.append(root.val)
+        if root.right:
+            list+=self.inorderTraversal(root.right)
+        
+        return list
