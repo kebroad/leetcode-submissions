8e58d3b589386ebc169744a0b93b05fab574cbe5
Kevin Broadbridge <kevinbroadbridge1@gmail.com>
2020-09-03 21:37:13 -0500

    2

--- 2.py
+++ 2.py
@@ -1,23 +1,45 @@
 # Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+# Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        l1 = reverse(l1)
-        l2 = reverse(l2)
+        root = current = ListNode()
+        carryover = 0
+        while l1 or l2 or carryover:
+            res = ListNode()
+            if l1:
+                carryover += l1.val
+                l1 = l1.next
+            if l2:
+                carryover += l2.val
+                l2 = l2.next
+            current.next = current = ListNode(carryover % 10)
+            if carryover >= 10:
+                carryover = 1
+            else:
+                carryover = 0
+        return root.next
+
+    
 
 
 
+    # def listToNum(self, head: ListNode) -> int:
+    #     res = int(head.val)
+    #     count = 0
+    #     head = head.next
+    #     while head.next is not None:
+    #         res = res + 10*count*head.val
+    #     return res
 
-    def reverse(self, l1: ListNode):
-        cur = l1.next
-        prev = l1
-        while cur.next is not None:
-            cur = cur.next
-            cur.next = prev
-            prev = prev.next
-
-        return prev
+s = Solution()
 
